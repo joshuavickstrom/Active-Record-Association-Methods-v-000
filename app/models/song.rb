df197c7affaac5bdbcc0d2d49412b9424e9848f6
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-03-13 17:07:12 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,9 @@
-require 'pry'
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
 
   def get_genre_name
     Artist.first(genre: "Rock")
-    binding.pry
   end
 
   def drake_made_this
